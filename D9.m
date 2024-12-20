dfc1dd3fb5e5d7a2005e04ec54bfcdc07e50a380
Aiman <aimandanish5308@gmail.com>
2024-12-20 02:16:30 +0000

    D9P2 attampt 1, too slow, would take ~1hr

--- D9.m
+++ D9.m
@@ -5,9 +5,14 @@ filename = "D9 Data.txt";
 diskMap = readlines(filename);
 
 blocks = decodeDiskMap(diskMap);
-compactedBlocks = compactDiskIndividualFiles(blocks);
-checksum = calculateChecksum(compactedBlocks);
-fprintf("Checksum: %i \n\n",checksum)
+compactedBlocksIndividual = compactDiskIndividualFileBlocks(blocks);
+checksumIndividualMethod = calculateChecksum(compactedBlocksIndividual);
+fprintf("Checksum (individual method): %i \n\n",checksumIndividualMethod)
+
+%% Part 2
+compactedBlocksWhole = compactDiskWholeFile(blocks);
+checksumWholeMethod = calculateChecksum(compactedBlocksWhole);
+fprintf("Checksum (whole method): %i \n\n",checksumWholeMethod)
 
 
 
@@ -51,11 +56,11 @@ end
 
 
 
-function blocks = compactDiskIndividualFiles(blocks)
+function blocks = compactDiskIndividualFileBlocks(blocks)
 locLastFileBlock = find(blocks ~= ".",1,"last");
 locFirstFreeSpaceBlock = find(blocks == ".",1,"first");
 
-fprintf("[%s] - Compacting disk (individual files).\n",datetime)
+fprintf("[%s] - Compacting disk (individual file blocks).\n",datetime)
 nIterations = 0;
 while locLastFileBlock > locFirstFreeSpaceBlock
     nIterations = nIterations + 1;
@@ -69,6 +74,68 @@ while locLastFileBlock > locFirstFreeSpaceBlock
     locFirstFreeSpaceBlock = find(blocks == ".",1,"first");
 end
 fprintf("[%s] - Compacting disk done. Iterations: %i\n",datetime,nIterations)
+end
+
+
+
+function blocks = compactDiskWholeFile(blocks)
+fprintf("[%s] - Compacting disk (whole file blocks).\n",datetime)
+fileBlocks = blocks(blocks ~= ".");
+fileIdArray = unique(str2double(fileBlocks)); % returns sorted array (ascending)
+fileIdArray = fileIdArray(end:-1:1); % sort in descending order
+fileIdArray = string(fileIdArray);
+
+% create struct to store more info together
+nBlocks = numel(blocks);
+blocksCell = num2cell(blocks);
+BlockData = struct;
+[BlockData(1:nBlocks).block] = blocksCell{:};
+[BlockData([BlockData.block] == ".").type] = deal("Free Space");
+[BlockData([BlockData.block] ~= ".").type] = deal("File");
+
+
+% delete this line
+% fileIdArray = fileIdArray(1:10);
+for thisFileId = fileIdArray(:)'
+    % disp(thisFileId) % delete this line
+    indxThisFileId = [BlockData.block] == thisFileId;
+    nBlocksThisFileId = sum(indxThisFileId,"all");
+    firstLocThisFileId = find(indxThisFileId,1,"first");
+
+    indxFreeSpace = [BlockData.type] == "Free Space";
+    [startLocsFreeSpace,~,nFreeSpaces] = groupLogicalIndices(indxFreeSpace);
+    
+    indxValidFreeSpaceGroups = startLocsFreeSpace < firstLocThisFileId & ...
+                               nFreeSpaces >= nBlocksThisFileId;
+
+    if ~any(indxValidFreeSpaceGroups)
+        continue
+    end
+
+    startLocsValidFreeSpace = startLocsFreeSpace(indxValidFreeSpaceGroups);
+    
+    startLocChosenFreeSpace = startLocsValidFreeSpace(1); % choose leftmost one
+    stopLocChosenFreeSpace = startLocChosenFreeSpace + nBlocksThisFileId - 1;
+
+    % swap
+    fileBlocksToSwap = BlockData(indxThisFileId);
+    freeSpaceBlocksToSwap = BlockData(startLocChosenFreeSpace:stopLocChosenFreeSpace);
+    BlockData(startLocChosenFreeSpace:stopLocChosenFreeSpace) = fileBlocksToSwap;
+    BlockData(indxThisFileId) = freeSpaceBlocksToSwap;
+end
+blocks = [BlockData.block];
+fprintf("[%s] - Compacting disk done. Iterations: %i\n",datetime,numel(fileIdArray))
+end
+
+
+
+function [startLocs,stopLocs,nElements] = groupLogicalIndices(indx)
+arguments
+    indx (1,:) logical
+end
+startLocs = find(diff([false indx]) == 1);
+stopLocs = find(diff([indx false]) == -1);
+nElements = stopLocs - startLocs + 1;
 end
 
 
