1550b635904f845fc4721adb7129886ad9cb5b32
unknown <aimandanish5308@gmail.com>
2025-12-05 23:57:58 +0000

    D04 Part 2

--- D04.m
+++ D04.m
@@ -2,18 +2,6 @@ clear; clc; close all;
 
 positions = readlines("D04_Data.txt");
 positions = positions(positions ~= "");
-
-% positions = ["..@@.@@@@."; ...
-%              "@@@.@.@.@@"; ...
-%              "@@@@@.@.@@"; ...
-%              "@.@@@@..@."; ...
-%              "@@.@@@@.@@"; ...
-%              ".@@@@@@@.@"; ...
-%              ".@.@.@.@@@"; ...
-%              "@.@@@.@@@@"; ...
-%              ".@@@@@@@@."; ...
-%              "@.@.@@@.@.";];
-
 positions = string(num2cell(char(positions)));
 isPaperRoll = positions == "@";
 
@@ -21,10 +9,25 @@ isPaperRoll = positions == "@";
 convMatrix = ones(3, 3);
 convMatrix(2,2) = 0;
 
-nSurroundingRolls = conv2(double(isPaperRoll), convMatrix);
+nSurroundingRolls = conv2(isPaperRoll, convMatrix);
 nSurroundingRolls = nSurroundingRolls(2:end-1, 2:end-1);
 
 isReachable = nSurroundingRolls < 4 & isPaperRoll;
 nReachableRolls = nnz(isReachable);
 
 fprintf("Number of reachable paper rolls = %i\n", nReachableRolls);
+
+%% Part 2
+nOriginalRolls = nnz(isPaperRoll);
+while nReachableRolls > 0
+    isPaperRoll = isPaperRoll .* ~isReachable;
+    nSurroundingRolls = conv2(isPaperRoll, convMatrix);
+    nSurroundingRolls = nSurroundingRolls(2:end-1, 2:end-1);
+
+    isReachable = nSurroundingRolls < 4 & isPaperRoll;
+    nReachableRolls = nnz(isReachable);
+end
+nCurrentRolls = nnz(isPaperRoll);
+nRemovableRolls = nOriginalRolls - nCurrentRolls;
+
+fprintf("Number of removable paper rolls = %i\n", nRemovableRolls);
