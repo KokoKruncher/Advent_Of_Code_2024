15a1444d00c45205e76e47b17a04e315e3ebff87
unknown <aimandanish5308@gmail.com>
2025-12-08 17:29:13 +0000

    D06 Part 2

--- D06.m
+++ D06.m
@@ -10,19 +10,73 @@ operators = problems(end,:);
 
 %% Part 1
 nProblems = width(problems);
-results = nan(1, nProblems);
+results1 = nan(1, nProblems);
 
 for ii = 1:nProblems
     thisOperator = operators(ii);
     switch thisOperator
         case "+"
-            results(ii) = sum(numbers(:,ii));
+            results1(ii) = sum(numbers(:,ii));
         case "*"
-            results(ii) = prod(numbers(:,ii));
+            results1(ii) = prod(numbers(:,ii));
         otherwise
             error("Unknown operator: %s", thisOperator);
     end
 end
 
-grandTotal = sum(results);
-fprintf("Grand total = %i\n", grandTotal);
+grandTotal1 = sum(results1);
+fprintf("Grand total, part 1 = %i\n", grandTotal1);
+
+%% Part 2
+txt = readlines("D06_Data.txt");
+txt = txt(txt ~= "");
+txt = convertStringsToChars(txt);
+
+numbers = txt(1:end-1);
+numbers = vertcat(numbers{:});
+operators = split(strip(convertCharsToStrings(txt(end))));
+nProblems = numel(operators);
+
+isSpace = numbers == ' ';
+isSeparator = all(isSpace, 1);
+iSeparator = find(isSeparator).';
+assert(numel(iSeparator) == (nProblems - 1));
+
+iSplit = [[1; iSeparator+1], [iSeparator-1; numel(isSeparator)]];
+assert(height(iSplit) == nProblems);
+splitNumbers = cell(nProblems, 1);
+
+nRows = height(numbers);
+for ii = 1:nProblems
+    rawNumbersArray = numbers(1:nRows, iSplit(ii,1):iSplit(ii,2));
+    rawNumbersArray = str2double(num2cell(rawNumbersArray));
+
+    nNumbers = width(rawNumbersArray);
+    parsedNumbers = nan(nNumbers, 1);
+    for jj = nNumbers:-1:1
+        thisColumn = rawNumbersArray(:,jj);
+        thisColumn = thisColumn(~isnan(thisColumn));
+        nDigits = numel(thisColumn);
+        thisColumn = thisColumn .* (10 .^ ((nDigits - 1):-1:0)).';
+        thisColumn = sum(thisColumn);
+
+        parsedNumbers(jj) = thisColumn;
+    end
+    splitNumbers{ii} = parsedNumbers(end:-1:1);
+end
+
+results2 = nan(1, nProblems);
+for ii = 1:nProblems
+    thisOperator = operators(ii);
+    switch thisOperator
+        case "+"
+            results2(ii) = sum(splitNumbers{ii});
+        case "*"
+            results2(ii) = prod(splitNumbers{ii});
+        otherwise
+            error("Unknown operator: %s", thisOperator);
+    end
+end
+
+grandTotal2 = sum(results2);
+fprintf("Grand total, part 2 = %i\n", grandTotal2);
