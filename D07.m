e4edb0d2a223cc07c94a4f961f14408f63ff622d
unknown <aimandanish5308@gmail.com>
2025-12-08 18:22:15 +0000

    D07 Part 2

--- D07.m
+++ D07.m
@@ -33,3 +33,38 @@ for ii = 2:nRows
 end
 
 fprintf("Number of times the beam will be split = %i\n", nHitsTotal);
+
+%% Part 2
+% Start with 1 possible path.
+% Each time a splitter is hit by a beam, it adds 1 possibility.
+
+% Beams are not merged. For example, in this scenario, the splitter in the 3rd row is hit twice, not once.
+% .....|.|.......
+% ....|^|^|......
+% ......^........
+
+isBeam = manifold == 'S';
+isBeam = isBeam(1,:);
+nBeams = double(isBeam);
+
+nPossibilities = 1;
+for ii = 2:nRows
+    isSplitter = isSplitterArray(ii,:);
+    nHits = isSplitter .* nBeams;
+
+    if ~any(nHits)
+        continue
+    end
+
+    nPossibilities = nPossibilities + sum(nHits);
+
+    % Delete beams that hit splitter
+    nBeams = nBeams - nHits;
+
+    % Split beams left and right
+    nSplitLeft = [nHits(2:end), 0];
+    nSplitRight = [0, nHits(1:end-1)];
+    nBeams = nBeams + nSplitLeft + nSplitRight;
+end
+
+fprintf("Number of possibilities = %i\n", nPossibilities);
