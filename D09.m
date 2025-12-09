6b4a9b7e96fa2d91b40d9b7afa15bdab96fa8463
unknown <aimandanish5308@gmail.com>
2025-12-09 23:37:12 +0000

    Optimised D09

--- D09.m
+++ D09.m
@@ -29,7 +29,11 @@ startCorners = coords(pairs(:,1),:);
 endCorners = coords(pairs(:,2),:);
 areas = squareAreas(startCorners, endCorners);
 
-% S, SV, E, EV
+[areas, iSort] = sort(areas, "descend");
+startCorners = startCorners(iSort,:);
+endCorners = endCorners(iSort,:);
+
+% Start, Start-Vertical, End, End-Vertical
 allCornersX = [startCorners(:,1), startCorners(:,1), endCorners(:,1), endCorners(:,1)];
 allCornersY = [startCorners(:,2), endCorners(:,2), endCorners(:,2), startCorners(:,2)];
 nSquares = height(pairs);
@@ -38,16 +42,19 @@ nSquares = height(pairs);
 % Can't be arsed to use vertex coordinates instead of center coordinates to make lines have > 0 area.
 % Heuristic based on plotting the boundary is that these lines won't be the solution for maximum area anyway.
 warning("off");
-for ii = nSquares:-1:1
-    squares(ii) = polyshape(allCornersX(ii,:), allCornersY(ii,:));
+for iMaxArea = 1:nSquares
+    square = polyshape(allCornersX(iMaxArea,:), allCornersY(iMaxArea,:));
+    nonIntersectingArea = square.xor(boundaryShape);
+    areaOutsideBoundary = nonIntersectingArea.subtract(boundaryShape);
+    isWithinBoundary = areaOutsideBoundary.NumRegions == 0;
+
+    if isWithinBoundary
+        break
+    end
 end
 warning("on");
 
-nonIntersectingArea = squares.xor(boundaryShape);
-areaOutsideBoundary = nonIntersectingArea.subtract(boundaryShape);
-nRegionsOutsideBoundary = [areaOutsideBoundary.NumRegions];
-isWithinBoundary = nRegionsOutsideBoundary == 0;
-maxArea = max(areas(isWithinBoundary));
+maxArea = areas(iMaxArea);
 
 toc
 
